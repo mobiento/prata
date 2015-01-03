948d9fe48e1efd521e53e6d4c7e2c14b3192684e
Julien Béchade <julien.bechade@gmail.com>
2015-01-03 04:25:21 +0100

    Added basic auth.

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
-var server = require('json-server');
+var server 	= require('json-server')
+var auth 	= require('http-auth')
+var app 	= server({ schemas: [] }, 'db.json')
 
-server({ schemas: [] }, 'db.json').listen(process.env.PORT || 3000);
+var basic = auth.basic({
+	realm: 'Requires authentication.',
+	file: __dirname + '/.htpasswd'
+})
+
+app.use(auth.connect(basic))
+app.listen(process.env.PORT || 3000)

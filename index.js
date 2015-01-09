22a8972d0921123f5c7ac0c77cae598975289951
Julien Béchade <julien.bechade@gmail.com>
2015-01-09 13:30:34 +0100

    Removed http-auth code.

--- index.js
+++ index.js
@@ -1,11 +1,4 @@
 var server 	= require('json-server')
-var auth 	= require('http-auth')
 var app 	= server({ schemas: [] }, 'db.json')
 
-var basic = auth.basic({
-	realm: 'Requires authentication.',
-	file: __dirname + '/.htpasswd'
-})
-
-app.use(auth.connect(basic))
 app.listen(process.env.PORT || 3000)

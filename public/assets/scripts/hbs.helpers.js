7ac9f6ae9c3ef9e72974dddb88327b19ac272666
Julien Béchade <julien.bechade@gmail.com>
2015-01-09 13:31:59 +0100

    Added some more helpers based on Faker.

--- public/assets/scripts/hbs.helpers.js
+++ public/assets/scripts/hbs.helpers.js
@@ -1,23 +1,54 @@
-Handlebars.registerHelper('_fullname', function () {
-	return Faker.name.findName();
-});
+(function (Handlebars, Faker) {
 
-Handlebars.registerHelper('_firstname', function () {
-	return Faker.name.firstName();
-});
+	function arrayFromArgs (args) {
+		var arr = [];
+		for(var i = 0; i < args.length - 1; ++i) { arr.push(args[i]); }
+		return arr;
+	}
 
-Handlebars.registerHelper('_lastname', function () {
-	return Faker.name.lastName();
-});
+	function setFakerLanguageFromArgs (args) {
+		if (typeof args[0] !== 'object') {
+			if (typeof args[1] === 'object') { Faker.locale = args[0]; }
+			else { Faker.locale = Faker.random.array_element(arrayFromArgs(args)); }		
+		}
+	}
 
-Handlebars.registerHelper('_city', function () {
-	return Faker.address.city();
-});
+	Handlebars.registerHelper('_fullname', function () {
+		setFakerLanguageFromArgs(arguments);
+		return Faker.name.findName();
+	});
 
-Handlebars.registerHelper('_state', function () {
-	return Faker.address.state();
-});
+	Handlebars.registerHelper('_firstname', function () {
+		setFakerLanguageFromArgs(arguments);
+		return Faker.name.firstName();
+	});
 
-Handlebars.registerHelper('_number', function (min, max, options) {
-	return Faker.random.number({min: min, max: max});
-});
+	Handlebars.registerHelper('_lastname', function () {
+		setFakerLanguageFromArgs(arguments);
+		return Faker.name.lastName();
+	});
+
+	Handlebars.registerHelper('_city', function () {
+		setFakerLanguageFromArgs(arguments);
+		return Faker.address.city();
+	});
+
+	Handlebars.registerHelper('_state', function () {
+		setFakerLanguageFromArgs(arguments);
+		return Faker.address.state();
+	});
+
+	Handlebars.registerHelper('_random', function () {
+		if (typeof arguments[0] === 'number') {
+			return Faker.random.number({
+				min: (arguments[1] && typeof arguments[1] === 'number') ? arguments[0] : 0, 
+				max: (arguments[1] && typeof arguments[1] === 'number') ? arguments[1] : arguments[0], 
+				precision: (arguments[2] && typeof arguments[2] === 'number') ? arguments[2] : 1
+			});
+		}
+		if (typeof arguments[0] === 'string') {
+			return Faker.random.array_element(arrayFromArgs(arguments));
+		}
+	});
+
+})(Handlebars, Faker);

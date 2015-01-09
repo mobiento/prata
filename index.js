8fbf4db21ca4d1d65851dc7474d3c8adc1000bed
Julien Béchade <julien.bechade@gmail.com>
2015-01-09 14:30:59 +0100

    Load clean data on server restart in production.

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
+var fs 		= require('fs')
 var server 	= require('json-server')
-var app 	= server({ schemas: [] }, 'db.json')
+var json
+var app
+
+if(process.env.NODE_ENV === 'production') {
+	json = JSON.parse(fs.readFileSync('db.json', 'utf8'))
+	app = server(json)
+}
+else {
+	app = server({}, 'db.json')
+}
+
 
 app.listen(process.env.PORT || 3000)

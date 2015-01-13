7dc4874a10bc9c26ab2ff6de81c67b60648125ea
Julien Béchade <julien.bechade@gmail.com>
2015-01-13 11:19:41 +0100

    Long story short: Mounted `json-server` properly and allowed `prata` to be mounted.

--- index.js
+++ index.js
@@ -1,15 +1,69 @@
-var fs 		= require('fs')
-var server 	= require('json-server')
-var json
-var app
+var fs             = require('fs');
+var express        = require('express');
+var server         = require('json-server');
+var cors           = require('cors');
+var bodyParser     = require('body-parser');
+var serveStatic    = require('serve-static');
+var methodOverride = require('method-override');
+var logger         = require('morgan');
+var errorhandler   = require('errorhandler');
 
-if(process.env.NODE_ENV === 'production') {
-	json = JSON.parse(fs.readFileSync('db.json', 'utf8'))
-	app = server(json)
+var log = console.log;
+var app = express();
+var options;
+var root;
+var json;
+
+if (!module.parent) {
+	server.use(logger('dev'));
+}
+
+app.use(bodyParser.json({limit: '10mb'}));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(methodOverride());
+app.use(cors({ origin: true, credentials: true }));
+
+if (process.env.NODE_ENV === 'development') {
+	app.use(errorhandler());
+}
+
+if (fs.existsSync(process.cwd() + '/public')) {
+	app.use(serveStatic(process.cwd() + '/public'));
+} 
+else {
+	app.use(serveStatic(__dirname + '/src/public'));
+}
+
+if (fs.existsSync(process.cwd() + '/server.json')) {
+	root = process.cwd();
+} 
+else {
+	root = __dirname;
+}
+
+try {
+	options = JSON.parse(fs.readFileSync(root + '/server.json'));
+} 
+catch (err) {
+	throw err;
+}
+
+if (process.env.NODE_ENV === 'development' && options.dev) {
+	options = options.dev;
+}
+
+if(options.db.object) {
+	try {
+		json = JSON.parse(fs.readFileSync(root + '/' + options.db.filename));
+	}
+	catch (err) {
+		throw err;
+	}
+	
+	app.use(server(json));
 }
 else {
-	app = server({}, 'db.json')
+	app.use(server({}, root + '/' + options.db.filename));
 }
 
-
-app.listen(process.env.PORT || 3000)
+module.exports = app;
